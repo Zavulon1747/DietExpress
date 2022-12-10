526d8e2ec7ef8d1892ee42d4a0405d53bb599bc0
Dmitriy Barbashov <d1mas322@yandex.ru>
2022-12-11 03:08:47 +0400

    Initial commit

---
+++ src/main/java/com/example/dietexpress/DietExpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietExpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dietexpress/DietExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
